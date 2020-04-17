94e0a6526dd817df85a36552e3f252ce897f9a55
Ubuntu <ubuntu@ip-172-31-34-44.ap-south-1.compute.internal>
2020-04-17 06:29:55 +0000

    msm8976: revert to base cpu clock

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 19 11:16:19 UTC 2020"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "ip-172-31-34-44"
+#define LINUX_COMPILER "gcc version 6.4.1 20180425 [linaro-6.4-2018.05 revision 7b15d0869c096fe39603ad63dc19ab7cf035eb70] (Linaro GCC 6.4-2018.05) "
