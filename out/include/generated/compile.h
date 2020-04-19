0b99a7a1d06f51db5f1fafad1ba9b8eac19d8a45
arnb70 <arnabpal70@gmail.com>
2020-04-19 05:41:46 +0000

    removed compile residual

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Sun Apr 19 05:32:42 UTC 2020"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 19 05:38:01 UTC 2020"
 #define LINUX_COMPILE_BY "ubuntu"
 #define LINUX_COMPILE_HOST "ip-172-31-34-44"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
