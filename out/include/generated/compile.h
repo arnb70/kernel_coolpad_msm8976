f8331613f1278111e320cef5675113f538346872
arnb70 <arnabpal70@gmail.com>
2020-04-19 04:34:57 +0000

    msm: fix build errrors

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 19 04:34:14 UTC 2020"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "ip-172-31-34-44"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
