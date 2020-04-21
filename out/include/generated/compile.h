ba4207c64188f18e9703e2df871ad884dcd209d9
arnb70 <arnabpal70@gmail.com>
2020-04-21 18:48:54 +0000

    fix error

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Tue Apr 21 17:59:20 UTC 2020"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "ip-172-31-16-122"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
