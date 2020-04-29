7a670a930ce1137ad47907c30610ac9fc3277e99
arnb70 <arnabpal70@gmail.com>
2020-04-29 12:40:05 +0000

    Revert "lmk: force disable adaptive lmk"
    This reverts commit 7362f95349aa5aabb385feaa80459a0ca49889c9.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -88,9 +88,8 @@ static atomic_t shift_adj = ATOMIC_INIT(0);
 static short adj_max_shift = 353;
 
 /* User knob to enable/disable adaptive lmk feature */
-static int enable_adaptive_lmk = 0;
-static int enable_adaptive_lmk_dummy;
-module_param_named(enable_adaptive_lmk, enable_adaptive_lmk_dummy, int,
+static int enable_adaptive_lmk;
+module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
 	S_IRUGO | S_IWUSR);
 
 /*
